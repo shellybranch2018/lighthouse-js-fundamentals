992f0272f67615d4d04b8ec6b97dc86291bef93c
shellybranch2018 <sbranch@homeaway.com>
2018-12-29 17:40:50 +0000

    Adds conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,55 @@
+/*var raining = false;
+var cold = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+*/
+/*
+var cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+*/
+/*
+var temperature = 12;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+*/
+/*
+var age = 19;
+
+var isCitizen = "Shell";
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+*/
+var temperature = 41;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+var raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
